5422ec4d44df175c33491b3c12780f3f20c5e5eb
dg314 <david_grossman@brown.edu>
2022-01-06 18:04:01 -0500

    about page

---
+++ src/content/David.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function David() {
+    return (
+        <>
+            <img className="object-contain w-full h-full" draggable="false" src="david.png" alt="David Portrait"/>
+        </>
+    );
+}
+
+export default David;
