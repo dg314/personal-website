a19ff54ba41b77fc941466724273b646b43cd651
dg314 <david_grossman@brown.edu>
2022-01-07 16:19:05 -0500

    color change

--- src/App.js
+++ src/App.js
@@ -69,10 +69,10 @@ function App() {
     
     return (
         <>
-            <div className="absolute w-full h-full opacity-20">
+            <div className="absolute w-full h-full opacity-50">
                 <img className="object-contain w-full h-full px-16 pb-16 pt-20" draggable="false" src="/pi_symbol.svg" alt="Pi Symbol"/>
             </div>
-            <div className="font-mono absolute flex items-center z-50 w-full h-8 bg-[#070707]">
+            <div className="font-mono absolute flex items-center z-50 w-full h-8 bg-black">
                 <img className="h-4 m-2 text-white" draggable="false" src="/pi_symbol_white.svg" alt="Pi Symbol"/>
                 <h1 className="text-center text-white text-sm mx-1">dgOS v3.14</h1>
                 <h1 className="text-center text-white text-sm ml-auto mr-2">{time}</h1>
